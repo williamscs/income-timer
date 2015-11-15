a44216be8303f71c9478052ed9d9f25e5bda943e
hovanem <matt.hovanec@autodesk.com>
2015-11-14 17:39:15 -0800

    money stopwatch

---
+++ index.js
@@ -0,0 +1,45 @@
+window.onload = function () {
+
+  var seconds = 0;
+
+  var appendSeconds = document.getElementById("seconds")
+  var appendMinutes = document.getElementById("minutes")
+  var appendHours = document.getElementById("hours")
+  var appendMoney = document.getElementById('money');
+  var hourlyRate = document.getElementById('hourlyRate');
+
+  var buttonStart = document.getElementById('button-start');
+  var buttonStop = document.getElementById('button-stop');
+  var buttonReset = document.getElementById('button-reset');
+
+  var Interval;
+
+  buttonStart.onclick = function() {
+    Interval = setInterval(startTimer, 1000);
+  }
+
+  buttonStop.onclick = function() {
+    clearInterval(Interval);
+  }
+
+
+  buttonReset.onclick = function() {
+    clearInterval(Interval);
+    seconds = "00";
+    appendSeconds.innerHTML = seconds;
+  }
+
+
+
+  function startTimer () {
+    var rate = Number(hourlyRate.value);
+    if (rate === NaN) {
+      rate = 0;
+    }
+
+    seconds++;
+    appendSeconds.innerHTML = "" + (parseInt(seconds / 10) % 6) + parseInt(seconds % 10);
+    appendMoney.innerHTML = "$" + (seconds / 60 / 60 * rate).toFixed(2);
+    //appendMoney.innerHTML = (((tens / 100 / 60 / 60) + (seconds / 60 / 60)) * hourlyRate).toFixed(2);
+  }
+}
